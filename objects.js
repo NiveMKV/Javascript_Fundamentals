5bc8ea6caf54a4d70cd2b18d315bb6bf593f96b6
NiveMKV <63994416+NiveMKV@users.noreply.github.com>
2021-06-11 16:45:02 +0530

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,13 +1,9 @@
-/**
- * 
-*/
 
 //Creating an empty object
 const person = {}
 
 //Object with Values.
-//The value of properties or keys 
-//could be a string, number, boolean, an object, null, undefined or a function.
+//The value of properties or keys could be a string, number, boolean, an object, null, undefined or a function.
 const user ={ firstName : "Max",
                 lastName : "Hellen",
                 age : 20,
@@ -24,10 +20,11 @@ const user ={ firstName : "Max",
                 'phone Number' : '+11 1234 567'
              }
 user.nationality = "Indian";//Setting the keys of an object.
-console.log(user.nationality)//getting th keys
+console.log(user.nationality)//getting the keys
+
 ///OBJECTS METHODS:
 
-//Copy the object without modyfying the original object.
+//Copy the object without modifying the original object.
 const copyUser = Object.assign({},user);
 console.log(copyUser);
 console.log(Object.entries(copyUser))
@@ -40,7 +37,7 @@ console.log(keyValues);
 const valuesOfKeys = Object.values(copyUser);
 console.log(valuesOfKeys);
 
-//Access object's keys ny dot operator.
+//Access object's keys using dot notation.
 console.log(user.getFullName());
 console.log(user.country); // dot operator.
 console.log(user['phone Number']); //when key has space in it,can't use dot operator.
