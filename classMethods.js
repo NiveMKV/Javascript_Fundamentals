83b6b057dee853a46b1367432af04b5b9b4e8623
NiveMKV <63994416+NiveMKV@users.noreply.github.com>
2021-06-11 16:20:44 +0530

    Creation of methods in a class

--- classMethods.js
+++ classMethods.js
@@ -1,8 +1,7 @@
 /**
  * Methods are JavaScript functions inside the class.
  * Static methods use static keyword which can be used as utility functions.
- * No constructor overloading
- * No function overloading (no CTE even in run time it doesn't throw error why ?!)
+ * No constructor overloading is supported in Javascript.
  */
  class Person {
     constructor(firstName, lastName, age, country, city) {
@@ -41,14 +40,8 @@
     constructor(firstName, lastName, age, country, city, gender) {
       super(firstName, lastName, age, country, city)
       this.gender = gender
-    }
-
-    getPersonInfo()
-    {
-        return 'heyy'
-    }
-    
-}
+    }    
+  }
 
   const person1 = new Person('Max', 'Hellen', 50, 'India', 'Chennai','75','htl');
   person1.setSkills='HTML'
@@ -56,7 +49,7 @@
   person1.setSkills='Javascript'
   console.log(person1.score)
   console.log(person1.skills)
-  person1.skills = ['html','css']
+  person1.skills = ['React.js','Node.js']
   console.log(person1.skills)
 
   const student = new Student('Harry', 'Potter', 25, 'US', 'California',"Male");
@@ -64,6 +57,6 @@
   console.log(student.getFullName())
   console.log(person1);
   console.log(person1.getFullName());
-  console.log(person1.getScore)//no need to use parantheses.
+  console.log(person1.getScore)//no need to use parantheses for getter methods.
   console.log(person1.skills);
   console.log(student.getPersonInfo());
