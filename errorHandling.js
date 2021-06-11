252d83cf38a034d6aec24074b3fa53c3fc7fb9e7
NiveMKV <63994416+NiveMKV@users.noreply.github.com>
2021-06-11 16:22:25 +0530

    Basics of error Handling

--- errorHandling.js
+++ errorHandling.js
@@ -4,16 +4,14 @@
 function showError(){
         try {
             let lastName = 'abc'
-            let fullName = fistName + ' ' + lastName
+            let fullName = firstName + ' ' + lastName
         } catch (err) {
-            //console.error(err) // we can use console.log() or console.error()
-            // console.log("Encountered an error "+err);
-            throw new Error('Custom err!!! message')
+            console.log("Encountered an error "+err);
         } finally {
             console.log('In any case I will be executed')
         }
         // throw 'Error2' // generates an exception with a string value
-        // throw 42 // generates an exception with the value 42
+        // throw 404 // generates an exception with the value 404
         // throw true // generates an exception with the value true
         // throw new Error('Required') // generates an error object with the message of Required
 }        
