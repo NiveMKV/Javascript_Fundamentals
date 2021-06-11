a6cdfcf8cd84a62ad0d89046a3c61e6a6fad4ee6
NiveMKV <63994416+NiveMKV@users.noreply.github.com>
2021-06-11 16:13:16 +0530

    Create, Access, Modify and Manipulate Arrays

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-//Creation of arrays.can even store array in a array.
+//Creation of empty array and arrays with values. Can even store array in a array.
 const arr =[];
 const arrayWithValues =["India","America","London"]
 const arraywithMixedDatatypes =[
@@ -10,42 +10,45 @@ const arraywithMixedDatatypes =[
                         [1,2,3,'4']
 ]
 console.log(arraywithMixedDatatypes)
+
 /**
  * To create an char array use split method of string.
  */
 let stringVariable ='Javascript';
 let charArray = stringVariable.split('');
-// console.log(charArray);
+console.log(charArray);
 
 /**
- * Access array using  Index
+ * Accessing arrays using its Index.
  */
-// console.log(charArray[2]);
+console.log(charArray[2]);
 
 /**
- * Modifying the array elements.
+ * Modifying the array elements using its Index.
  */
 charArray[2]='v';
-// console.log(charArray[2]);
-// console.log(charArray);
+ console.log(charArray[2]);
+ console.log(charArray);
 
 /**
  * Methods to manipulate array.
  * length, concat, indexOf, slice, splice, join, toString, includes, reverse, 
  * lastIndexOf, isArray, fill, push, pop, shift, unshift
  */
-//  console.log(charArray.length);
+ console.log(charArray.length);
 
+//Join two arrays using Concat method.
  const firstArray =[1,2,3]
  const secondArray =['a','b',3]
 const thirdArray = firstArray.concat(secondArray);
-// console.log(thirdArray);
+ console.log(thirdArray);
 
+//Find Index of an element present in an array.
 let index = thirdArray.indexOf('c');
-// console.log(index);
+console.log(index);
 
 let lastIndex = thirdArray.lastIndexOf(3);
-// console.log(lastIndex);
+console.log(lastIndex);
 
 if(index != -1){
     console.log('"c" does exist in the array')  
@@ -53,45 +56,45 @@ if(index != -1){
     console.log('"c" does not exist in the array')
 }
 
-//To check if element exists and to check if its an array.
-// console.log(thirdArray.includes('A'));
-// console.log(Array.isArray(lastIndex));
+//To check if an element exists and to check if its an array.
+console.log(thirdArray.includes('A'));
+console.log(Array.isArray(lastIndex));
 
 //Convert to string.
 console.log(thirdArray.toString())
 
-//Join yeilds string and joins array based on parameter supplied.
+//Join yeilds string in return and joins array based on delimiter supplied.
 console.log(thirdArray.join(""));
 
-//Slice. a part of original array.
+//Slice. a part of original array and return sliced array. It won't modify the original array.
 const slicedArray = thirdArray.slice(3,5);
 console.log(slicedArray);
 
-//Splice (can add also remove)
-//remove:
+//Splice (can Add also Remove elements based on the parameters supplied.)
+//remove an element using Splice:
 thirdArray.splice(5,1,'c');
 console.log(thirdArray);
 
-//add:
+//add an element using Splice:
 console.log(thirdArray)
 thirdArray.splice(5,0,'X','Y')
 console.log(thirdArray);
 
-//to add an element usinf push.
+//push :to add an element at the last Index.
 firstArray.push('4');
 console.log(firstArray);
 
-//pop
+//pop :to remove an element at the first Index.
 firstArray.pop('4')
 console.log(firstArray);
 
 //Sort and reverse.
+//The aforementioned functions work perfectly with strings. For numbers with float values, have to use comparefunction in the constructor.
 const ascSortArray = [1,2,3,4,5]
 console.log(ascSortArray.sort())
 console.log(ascSortArray.reverse())
 
-const plz = [3.85,100,40, 25]
-console.log(plz.sort())
+
 
 
 
